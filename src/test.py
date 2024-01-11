3f5ce0723c855833a319230717efd4cc96c8849a
tk231 <49594308+tk231@users.noreply.github.com>
2024-01-11 16:32:11 +0100

    Update test.py
    Update to own version of test.py

--- src/test.py
+++ src/test.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+import os
 import skimage.io as skio
 
 from tqdm import tqdm
@@ -23,15 +24,15 @@ def validate(test_dataloader, model):
         model.eval()
         # initialize denoised stack to NaN array.
         denoised_stack = np.zeros(test_dataloader.dataset.noisy_image.shape, dtype=np.float32)
-        
+
         # stitching denoised stack
         # insert the results if the stack value was NaN
         # or, half of the output volume
         for _, (noisy_image, _, single_coordinate) in enumerate(tqdm(test_dataloader, desc="validate")):
-            noisy_image = noisy_image.cuda() #[b, z, y, x]
+            noisy_image = noisy_image.cuda()  # [b, z, y, x]
             noisy_image_denoised = model(noisy_image)
             T = noisy_image.size(1)
-            for bi in range(noisy_image.size(0)): 
+            for bi in range(noisy_image.size(0)):
                 stack_start_w = int(single_coordinate['stack_start_w'][bi])
                 stack_end_w = int(single_coordinate['stack_end_w'][bi])
                 patch_start_w = int(single_coordinate['patch_start_w'][bi])
@@ -43,8 +44,8 @@ def validate(test_dataloader, model):
                 patch_end_h = int(single_coordinate['patch_end_h'][bi])
 
                 stack_start_s = int(single_coordinate['init_s'][bi])
-                
-                denoised_stack[stack_start_s+(T//2), stack_start_h:stack_end_h, stack_start_w:stack_end_w] \
+
+                denoised_stack[stack_start_s + (T // 2), stack_start_h:stack_end_h, stack_start_w:stack_end_w] \
                     = noisy_image_denoised[bi].squeeze()[patch_start_h:patch_end_h, patch_start_w:patch_end_w].cpu()
 
         # change nan values to 0 and denormalize
@@ -54,28 +55,54 @@ def validate(test_dataloader, model):
 
 
 if __name__ == '__main__':
+    import glob
+    import tifffile
+
     ########## Change it with your data ##############
     data_file = "./data/Xiaohan/sample/cropped2-2.tif"
-    model_file = "./src/GUI/trained_models/bs1.pth" # "./results/saved_models/mytest/model_0.pth"
+    model_file = "./src/GUI/trained_models/bs1.pth"  # "./results/saved_models/mytest/model_0.pth"
     output_file = "./results/xiaohan_denoised_0.tif"
+
     patch_size = [61, 64, 64]
     patch_interval = [1, 32, 32]
-    batch_size = 16    # lower it if memory exceeds.
-    bs_size = 3    # modify if you changed bs_size when training.
+    batch_size = 16  # lower it if memory exceeds.
+    bs_size = 1  # modify if you changed bs_size when training.
     ##################################################
 
-    model = SUPPORT(in_channels=61, mid_channels=[16, 32, 64, 128, 256], depth=5,\
-            blind_conv_channels=64, one_by_one_channels=[32, 16], last_layer_channels=[64, 32, 16], bs_size=bs_size).cuda()
+    os.makedirs(f"{output_folder}", exist_ok=True)
+
+    model = SUPPORT(in_channels=61, mid_channels=[16, 32, 64, 128, 256], depth=5, blind_conv_channels=64,
+                    one_by_one_channels=[32, 16], last_layer_channels=[64, 32, 16],
+                    bs_size=bs_size).cuda()
 
     model.load_state_dict(torch.load(model_file))
 
-    demo_tif = torch.from_numpy(skio.imread(data_file).astype(np.float32)).type(torch.FloatTensor)
-    demo_tif = demo_tif[:, :, :]
+    list_of_volumes_to_denoise = glob.glob(f"{data_folder}/*.tif")
+    # list_of_volumes_to_denoise = glob.glob(f"{data_folder}/*.tiff")
 
-    testset = DatasetSUPPORT_test_stitch(demo_tif, patch_size=patch_size,\
-        patch_interval=patch_interval)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size)
-    denoised_stack = validate(testloader, model)
+    for img_file in list_of_volumes_to_denoise:
+        # demo_tif = torch.from_numpy(skio.imread(data_file).astype(np.float32)).type(torch.FloatTensor)
+        demo_tif = torch.from_numpy(skio.imread(img_file).astype(np.float32)).type(torch.FloatTensor)
+        demo_tif = demo_tif[:, :, :]
 
-    print(denoised_stack.shape)
-    skio.imsave(output_file, denoised_stack[(model.in_channels-1)//2:-(model.in_channels-1)//2, : , :], metadata={'axes': 'TYX'})
+        testset = DatasetSUPPORT_test_stitch(demo_tif, patch_size=patch_size,
+                                             patch_interval=patch_interval)
+        testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size)
+        denoised_stack = validate(testloader, model)
+
+        print(denoised_stack.shape)
+
+        ##################################################
+
+        # Create savepath for file
+        file_basename = os.path.basename(img_file).split('.')[0]
+        output_designator = "_denoised"
+        file_ext = '.tif'
+        output_filepath = os.path.join(output_folder, f"{file_basename}{output_designator}{file_ext}")
+
+        ##################################################
+
+        # skio.imsave(output_filepath, denoised_stack[(model.in_channels - 1) // 2:-(model.in_channels - 1) // 2, :, :],
+        #             metadata={'axes': 'TYX'})
+        tifffile.imwrite(output_filepath, denoised_stack[(model.in_channels - 1) // 2:-(model.in_channels - 1) // 2, :, :], photometric='minisblack', metadata={'axes': 'ZYX'})
+
